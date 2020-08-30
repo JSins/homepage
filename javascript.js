19600a8a05e532eb5289ad026375af8b0e4e6942
JSins <nutzlosundso@gmail.com>
2020-08-30 17:52:50 +0200

    smoothscrollfix

--- javascript.js
+++ javascript.js
@@ -6,27 +6,25 @@ const scrollToTop = () => {
   }
 };
 
-$('#pageup').click(function(){
-  scrollToTop();
-})
 
 
 
-
-// $(window).scroll(function(){
-//     var y = $(this).scrollTop();
-//     if (y > 600) {
-//       $('#pageup').fadeIn();
-//     } else {
-//       $('#pageup').fadeOut();
-//     }
-//   });
+$(window).scroll(function(){
+    var y = $(this).scrollTop();
+    if (y > 600) {
+      $('#pageup').fadeIn();
+    } else {
+      $('#pageup').fadeOut();
+    }
+  });
 
 // $('#pageup').click(function(){
 //     $(window).scrollTop(0);
 // })
 
-
+$('#pageup').click(function(){
+  scrollToTop();
+})
 
 
 
